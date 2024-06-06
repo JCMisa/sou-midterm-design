46e9b012941f1f1a8a63e7d910a0f11b405a5788
MisaJC <123918601+JCMisa@users.noreply.github.com>
2024-06-06 12:04:47 +0800

    Add files via upload

---
+++ sou-initial-design/js/AuthForm.js
@@ -0,0 +1,21 @@
+﻿const auth_sign_in_btn = document.querySelector("#auth-sign-in-btn");
+const auth_sign_up_btn = document.querySelector("#auth-sign-up-btn");
+const auth_container = document.querySelector(".auth-container");
+const auth_sign_in_btn2 = document.querySelector("#auth-sign-in-btn2");
+const auth_sign_up_btn2 = document.querySelector("#auth-sign-up-btn2");
+
+auth_sign_up_btn.addEventListener("click", () => {
+    auth_container.classList.add("auth-sign-up-mode");
+});
+
+auth_sign_in_btn.addEventListener("click", () => {
+    auth_container.classList.remove("auth-sign-up-mode");
+});
+
+auth_sign_up_btn2.addEventListener("click", () => {
+    auth_container.classList.add("auth-sign-up-mode2");
+});
+
+auth_sign_in_btn2.addEventListener("click", () => {
+    auth_container.classList.remove("auth-sign-up-mode2");
+});
---
+++ sou-initial-design/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    toggle.addEventListener('click', () => {
+        // Add show-menu class to nav menu
+        nav.classList.toggle('n-show-menu')
+
+        // Add show-icon to show and hide the menu icon
+        toggle.classList.toggle('n-show-icon')
+    })
+}
+
+showMenu('n-nav-toggle', 'n-nav-menu')
